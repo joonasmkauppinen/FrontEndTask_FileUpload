a19c0773e6d267e2fd3348866fe63e18b0ffe3eb
joonasmkauppinen <joonasm.kauppinen@gmail.com>
2018-11-06 20:59:17 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const fs = require('fs');
+const multiparty = require('multiparty');
+const express = require('express');
+const app = express();
+const port = 8000;
+
+const isEmpty = (obj) => {
+	return (Object.keys(obj).length === 0 && obj.constructor === Object);
+}
+const newImageJson = (url) => {
+	return {"imgUrl": url}
+}
+
+let imagesJson = JSON.parse(fs.readFileSync(__dirname+'/public/images/images.json', 'utf-8'));
+
+
+app.use(express.static('public'));
+
+app.post('/file-upload/new-file', (req, res) => {
+	
+	let form = new multiparty.Form();
+	
+	form.parse(req, (err, fields, files) => {
+		if (isEmpty(files)) {
+			return;
+		}
+		let newImage = files.newfile[0];
+		let filename = newImage.originalFilename;
+		let oldpath = newImage.path;
+		let newpath = __dirname+'/public/images/'+filename;
+		let newImageUrl = 'http://localhost:8000/images/'+filename;
+		fs.rename(oldpath, newpath, (err) => {
+			if (err) {
+				res.send(err);
+			}
+			imagesJson.push(newImageJson(newImageUrl));
+			let data = JSON.stringify(imagesJson, null, 2);
+			fs.writeFileSync(__dirname+'/public/images/images.json', data);
+			res.send(newImageUrl);
+		});
+	});
+	
+});
+
+app.listen( port, () => console.log(`Server running on port ${port}...`));
---
+++ public/file-upload/scripts/main.js
@@ -0,0 +1,68 @@
+const imagesJsonUrl = 'http://localhost:8000/images/images.json';
+const form = document.getElementById('uploadForm');
+const imageList = document.getElementById('image-list');
+
+const showUploadedImage = (imgUrl) => {
+	console.log('updating')
+	const imgContainer = document.getElementById('uploadedImage');
+	imgContainer.src = imgUrl;
+	
+}
+
+const newListItem = (url) => {
+	const img = document.createElement("img");
+	img.setAttribute("src", url);
+	
+	const a = document.createElement("a");
+	a.setAttribute("href", url);
+	a.appendChild(img);
+
+	const li = document.createElement("li");
+	li.appendChild(a);
+
+	return li;
+}
+
+const showImage = (url) => {
+	imageList.insertBefore( newListItem(url), imageList.childNodes[0] );
+}
+
+// Fetch images stored on server.
+fetch(imagesJsonUrl)
+	.then( res => {
+		return res.json();
+	})
+	.then( data => {
+		data.forEach(element => {
+			showImage(element.imgUrl);
+		});
+	})
+	.catch( err => {
+		console.log(err);
+	});
+
+	
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+	
+	console.log("uploading to server...");
+	const fileInput = document.querySelector('input[type="file"]');
+	
+	const data = new FormData();
+	data.append('newfile', fileInput.files[0]);
+	console.log(data);
+	const settings = {
+		method: 'POST',
+		body: data
+	};
+	
+	fetch('http://localhost:8000/file-upload/new-file', settings)
+	.then( res => {
+		return res.text();
+	})
+	.then( url => {
+		showImage(url);
+		console.log(`Uploaded image to url: ${url}`);
+	});
+	
+});
